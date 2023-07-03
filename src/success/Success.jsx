3c12b92e5925804510950ace52a7df0ed21f6559
Levine Wang <wang_levine@outlook.com>
2023-07-03 11:55:19 -0700

    Scroll to top upon redirect

--- src/success/Success.jsx
+++ src/success/Success.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Hero from "./Hero";
 
 const Success = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       {" "}
